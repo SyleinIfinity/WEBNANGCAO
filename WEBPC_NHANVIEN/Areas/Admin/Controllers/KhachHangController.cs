b1305f71ad528ec88d77bff5db7be5f2c1663811
Potatomine725 <WotblitzGamer725@gmail.com>
2025-12-06 12:22:03 +0700

    Fix css calling

---
+++ WEBPC_NHANVIEN/Areas/Admin/Controllers/KhachHangController.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WEBPC_NHANVIEN.Areas.Admin.Models;
+
+namespace WEBPC_NHANVIEN.Areas.Admin.Controllers
+{
+    public class KhachHangController : Controller
+    {
+        private readonly string _baseUrl = "https://webapi-1-qldr.onrender.com/api/";
+
+        public async Task<ActionResult> Index()
+        {
+            var danhSach = new List<KhachHangViewModel>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(_baseUrl);
+                var response = await client.GetAsync("KhachHang");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var data = await response.Content.ReadAsStringAsync();
+                    danhSach = JsonConvert.DeserializeObject<List<KhachHangViewModel>>(data);
+                }
+            }
+            return View(danhSach);
+        }
+    }
+}
---
+++ WEBPC_NHANVIEN/Areas/Admin/Controllers/NhanVienController.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WEBPC_NHANVIEN.Areas.Admin.Models;
+
+namespace WEBPC_NHANVIEN.Areas.Admin.Controllers
+{
+    public class NhanVienController : Controller
+    {
+        private readonly string _baseUrl = "https://webapi-1-qldr.onrender.com/api/";
+
+        // GET: Admin/NhanVien
+        public async Task<ActionResult> Index()
+        {
+            var danhSach = new List<NhanVienViewModel>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(_baseUrl);
+                var response = await client.GetAsync("NhanVien"); // Gọi API GET All Nhân viên
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var data = await response.Content.ReadAsStringAsync();
+                    danhSach = JsonConvert.DeserializeObject<List<NhanVienViewModel>>(data);
+                }
+            }
+            return View(danhSach);
+        }
+
+        // Bạn có thể thêm Action Create/Edit/Delete ở đây để gọi API tương ứng
+    }
+}
