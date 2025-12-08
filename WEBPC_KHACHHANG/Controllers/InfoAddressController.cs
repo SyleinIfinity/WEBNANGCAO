18f13c3a4621c429acb03e59223bef94c2e3946c
nhatminh104 <hanhatminh1042005@gmail.com>
2025-12-08 17:35:48 +0700

    giao dien user-address

---
+++ WEBPC_KHACHHANG/Controllers/InfoAddressController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WEBPC_KHACHHANG.Controllers
+{
+    public class InfoAddressController : Controller
+    {
+        // GET: InfoAddress
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
