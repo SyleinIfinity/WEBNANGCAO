164cfbf00609d8071b7d9f688db3d14f370c179c
nhatminh104 <hanhatminh1042005@gmail.com>
2025-12-17 19:41:51 +0700

    update promotions in auth flow

---
+++ WEBPC_KHACHHANG/Models/ViewModels/PromotionsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using WEBPC_KHACHHANG.Models.Responses;
+
+namespace WEBPC_KHACHHANG.Models.ViewModels
+{
+    public class PromotionsViewModel
+    {
+        public List<KhuyenMaiResponse> TatCaKhuyenMai { get; set; }
+
+        // ✅ CHỈ LƯU DANH SÁCH ID MÃ ĐÃ LƯU
+        public List<int> KhoKhuyenMaiDaLuu { get; set; }
+    }
+}
